ed558181eb6a3cad3f81862a155af3e194481e34
Pavol Vargovcik <pallly.vargovcik@gmail.com>
2015-05-20 15:49:46 +0200

    better names for scripts

--- zmq_load_test_starter.py
+++ zmq_load_test_starter.py
@@ -1,41 +1,31 @@
-import subprocess, time, zmq, json
+import subprocess, time, zmq, json, sys
+from threading import Thread
 
-def demogrify(topicmsg):
-    """ Inverse of mogrify() """
-    json0 = topicmsg.find('{')
-    topic = topicmsg[0:json0].strip()
-    msg = json.loads(topicmsg[json0:])
-    return topic, msg
+def mogrify(topic, msg):
+    """ json encode the message and prepend the topic """
+    return topic + ' ' + json.dumps(msg)
 
-def measurement(params):
-  tic = time.clock()
+ctx = zmq.Context()
 
-  ps = []
-  for i in range(int(params['processes'])):
-    ps.append(
-      subprocess.Popen([ 'python3', 'zmq_load_test.py'
-                       , str(int(params['sockets']))
-                       , str(int(params['messages']))
-                       , str(int(params['max_length']))
-                       ])
-    )
+sender = ctx.socket(zmq.PUB)
+sender.bind('tcp://*:5557')
 
-  for p in ps:
-    p.wait()
+receiver = ctx.socket(zmq.PULL)
+receiver.bind('tcp://*:5558')
 
-  return time.clock() - tic
+def senderThread():
+  time.sleep(2)
+  sender.send_string(mogrify('measurement_start'
+                            , { 'processes':  int(sys.argv[2])
+                              , 'sockets':    int(sys.argv[3])
+                              , 'messages':   int(sys.argv[4])
+                              , 'max_length': int(sys.argv[5])
+                              }))
 
-if __name__ == '__main__':
-  ctx = zmq.Context()
+Thread(target=senderThread).start()
 
-  receiver = ctx.socket(zmq.SUB)
-  receiver.connect('tcp://192.168.14.204:5557')
-  receiver.setsockopt_string(zmq.SUBSCRIBE, 'measurement_start')
+res = []
+for _ in range(int(sys.argv[1])):
+  res.append(receiver.recv_string())
 
-  sender = ctx.socket(zmq.PUSH)
-  sender.connect('tcp://192.168.14.204:5558')
-
-  while True:
-    _, params = demogrify(receiver.recv_string())
-    comm_time = measurement(params)
-    sender.send_string(str(comm_time))
+print(max(map(float, res)))
